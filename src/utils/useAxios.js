ddd17cf26a0a29d2bdae03cc0257afa3a697315b
Devin Raleigh <devinkr@gmail.com>
2022-07-30 15:50:11 -0600

    Fix postAPIData and deleteAPIData calling wrong callback

--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -26,3 +26,49 @@ export async function getAPIData(endpoint) {
 		}
 	}
 }
+
+export async function postAPIData(endpoint, data) {
+	try {
+		const response = await axios.post(`${API_URL}/${endpoint}/`, data, {
+			headers: getAuthHeader(),
+		});
+		if (response.status === 201) {
+			return response;
+		}
+	} catch (error) {
+		if (
+			error.response.status === 401 &&
+			error.response.data.code === 'token_not_valid'
+		) {
+			const refresh = await refreshToken();
+			if (refresh.status === 200) {
+				return postAPIData(endpoint, data);
+			} else {
+				return refresh;
+			}
+		}
+	}
+}
+
+export async function deleteAPIData(endpoint) {
+	try {
+		const response = await axios.delete(`${API_URL}/${endpoint}/`, {
+			headers: getAuthHeader(),
+		});
+		if (response.status === 204) {
+			return response;
+		}
+	} catch (error) {
+		if (
+			error.response.status === 401 &&
+			error.response.data.code === 'token_not_valid'
+		) {
+			const refresh = await refreshToken();
+			if (refresh.status === 200) {
+				return deleteAPIData(endpoint);
+			} else {
+				return refresh;
+			}
+		}
+	}
+}
